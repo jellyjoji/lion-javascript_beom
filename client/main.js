0eac000688cb82fb76077eb528c4a5b1eefb34f8
kindtiger <seonbeom2@gmail.com>
2023-07-25 09:18:43 +0900

    add. 15일차 수업
    -memo closure 적용

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 
 
-import { attr, clearContents, diceAnimation, endScroll, getNode, getNodes, insertLast } from "./lib/index.js";
+import { attr, clearContents, diceAnimation, endScroll, getNode, getNodes, insertLast, memo } from "./lib/index.js";
 
 
 // [phase-1] 주사위 굴리기
@@ -34,7 +34,7 @@ import { attr, clearContents, diceAnimation, endScroll, getNode, getNodes, inser
 
 const [startButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
 const recordListWrapper = getNode('.recordListWrapper');
-const tbody = getNode('.recordList tbody');
+memo('@tbody',()=> getNode('.recordList tbody')) // setter
 
 
 // 진짜 진짜 쉬운 과제
@@ -64,10 +64,9 @@ function createItem(value){
 function renderRecordItem(){
 
   // 큐브의 data-dice 값 가져오기
-  const diceValue = +attr('#cube','data-dice');
+  const diceValue = +attr(memo('cube'),'data-dice');
 
-  insertLast(tbody,createItem(diceValue));
-
+  insertLast(memo('@tbody'),createItem(diceValue));
   endScroll(recordListWrapper);
 
 }
@@ -110,7 +109,7 @@ function handleReset(){
   recordButton.disabled = true;
   resetButton.disabled = true;
 
-  clearContents(tbody);
+  clearContents(memo('@tbody'));
   
   count = 0;
   total = 0;
