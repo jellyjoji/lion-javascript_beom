efe397e07b283cad18d1f43b7066338d0f3790b8
kindtiger <seonbeom2@gmail.com>
2023-07-27 11:54:42 +0900

    add. 17일차 수업
    - empty card 랜더링

--- client/main.js
+++ client/main.js
@@ -1,50 +1,65 @@
+/* global gsap */
 
+import { 
+  tiger,
+  delayP,
+  insertLast, 
+  getNode as $, 
+  changeColor,
+  renderSpinner,
+  renderUserCard,
+  renderEmptyCard,
+ } from './lib/index.js';
 
-import { insertLast, tiger } from './lib/index.js';
-
-
-
-
-
+// [phase-1]
 // 1. tiger 함수를 사용해서 user를 가져와 주세요.
 // 2. 함수 안으로 넣기
 // 3. 유저 데이터 랜더링 
 //      - html template을 만든다. 
 //      - 유저 data를 넘겨주기.
 //      - inserLast 사용하기.
+// 4. 함수 분리 하기 
+
+
+
+// 에러가 발생 했을 때 
+// empty svg를 생성하고 랜더링 해주세요 
+
+
+const userCardInner = $('.user-card-inner');
 
 async function renderUserList(){
-  const response = await tiger.get('https://jsonplaceholder.typicode.com/users')
+  renderSpinner(userCardInner)
+  try{
 
-  const userData = response.data;
+    await delayP({timeout:2000});
 
-  
-  userData.forEach((item)=>{
-    console.log( item );
-      const template = `
-      <article class="user-card" data-index="user-1">
-        <h3 class="user-name">kindtiger</h3>
-        <div class="user-resouce-info">
-          <div>
-            <a class="user-email" href="mailto:tiger@euid.dev">tiger@euid.dev</a>
-          </div>
-          <div>
-            <a class="user-website" href="http://tiger.com" target="_blank" rel="noopener noreferer">tiger.com</a>
-          </div>
-        </div>
-        <button class="delete">삭제</button>
-      </article>
-    `
+    gsap.to('.loadingSpinner',{
+      opacity:0,
+      onComplete(){
+        $('.loadingSpinner').remove();
+      }
+    })
+    const response = await tiger.get('https://jsonplaceholder.typicode.com/user')
+    const userData = response.data;
 
-    insertLast('.user-card-inner',template)
-  })
+    userData.forEach((item)=> renderUserCard(userCardInner,item))
 
+    changeColor('.user-card');
 
+    gsap.to('.user-card',{
+      x:0,
+      opacity:1,
+      stagger:0.1
+    })
+  }
 
+  catch(err){
+    console.log( err );
+    renderEmptyCard(userCardInner)
+    // location.href = '404.html'
+  }
 }
-
-
-
 
 
 renderUserList()
